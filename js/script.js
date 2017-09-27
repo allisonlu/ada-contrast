a870c56ea7931b692b48039683dec0d3cee227fd
SUPPORT\alu <allisonlu820@gmail.com>
2017-09-27 13:23:39 -0700

    added documentation and instructions, changed default palette to brand colors

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-// given a string hex, converts hex to rgb, returns an array
+/* Spectrum is a project by Brian Grinstead at https://bgrins.github.io/spectrum/ */
+
+/* given a string hex, converts hex to rgb, returns an array */
 function hexToRgb(hex) {
     while (hex.charAt(0)=='#') {
       hex = hex.substr(1);
@@ -14,7 +16,7 @@ function hexToRgb(hex) {
     return [r,g,b];
 }
 
-// rounding a decimal 2 places
+/* given a number in decimal format, the round function returns it with 2 digits after the decimal point */
 function round(decimal) {
   return parseFloat(Math.round(decimal * 100) / 100).toFixed(2);
 }
@@ -49,7 +51,7 @@ $.fn.slideFadeToggle = function(easing, callback) {
 
 /*****************************************************/
 
-// given some rgb value, converts standard rgb to values ready for luminance calculation, returns number
+/* given some rgb value, the getStandard function converts standard rgb to values ready for luminance calculation, returns number */
 function getStandard(val) {
 	var standard = val/255;
 
@@ -63,7 +65,7 @@ function getStandard(val) {
 	return standard;
 }
 
-// given a hex value, calculates relative luminance, returns a number
+/* given a hex value, calculates relative luminance, returns a number */
 function getLuminance(hex) {
   var rgb = hexToRgb(hex); // array
 
@@ -74,8 +76,8 @@ function getLuminance(hex) {
 	return (0.2126*red)+(0.7152*green)+(0.0722*blue);
 }
 
-/* given hex values of color1 and color2, where color1 has higher luminance, color2 has lower
-    returns number */
+/* given hex values of color1 and color2, where color1 has higher luminance, color2 has lower,
+    returns a number rounded to two decimal places */
 function contrastRatio(color1, color2) {
 	var ratio;
 	luminance1 = getLuminance(color1);
@@ -91,7 +93,7 @@ function contrastRatio(color1, color2) {
 	return round(ratio);
 }
 
-// determines whether contrast ratio is passing according to ada web standards
+/* given a contrast ratio, determines whether the ratio is passing according to ada web standards, returns a string */
 function isPassing(ratio) {
   if (ratio >= 7.99) {
     return "Passing - Level AAA";
@@ -119,13 +121,8 @@ $(document).ready(function() {
       togglePaletteLessText: 'less',
       palette: [
           ["#000","#444","#666","#999","#ccc","#eee","#f3f3f3","#fff"],
-          ["#f00","#f90","#ff0","#0f0","#0ff","#00f","#90f","#f0f"],
-          ["#f4cccc","#fce5cd","#fff2cc","#d9ead3","#d0e0e3","#cfe2f3","#d9d2e9","#ead1dc"],
-          ["#ea9999","#f9cb9c","#ffe599","#b6d7a8","#a2c4c9","#9fc5e8","#b4a7d6","#d5a6bd"],
-          ["#e06666","#f6b26b","#ffd966","#93c47d","#76a5af","#6fa8dc","#8e7cc3","#c27ba0"],
-          ["#c00","#e69138","#f1c232","#6aa84f","#45818e","#3d85c6","#674ea7","#a64d79"],
-          ["#900","#b45f06","#bf9000","#38761d","#134f5c","#0b5394","#351c75","#741b47"],
-          ["#600","#783f04","#7f6000","#274e13","#0c343d","#073763","#20124d","#4c1130"]
+          ["#3284bf", "#0054a6", "#00a5e5", "#20c4f4", "#347bad"],
+          ["#ffc828", "	#ffe600", "#fff200"]
       ]
   });
 
@@ -146,7 +143,7 @@ $(document).ready(function() {
       $('td:last-child').addClass('cell').addClass(headers[i]);
     }
 
-    // takes care of bug: last cell contains all headers as classes
+    // takes care of bug where last cell contains all headers as classes
     if ($('td:last-child').hasClass('hex rgb sample background ratio')) {
       $('td:last-child').removeClass("hex rgb sample background ratio");
     }
@@ -162,7 +159,6 @@ $(document).ready(function() {
     $('.rgb').each( function (i) {
       $(this).html("<p>(" + hexToRgb(colors[i]) + ")</p>");
     });
-    console.log(colors[i]);
 
     $('.sample').each( function (i) {
       $(this).css('background-color', colors[i]);
@@ -172,12 +168,11 @@ $(document).ready(function() {
       $(this).html("<p>TEST</p>");
       $(this).css('color', colors[i]);
     });
-    
+
    });
 
 
-  // updateColor updates color of background cell as user chooses a color
-  // also updates contrast ratio and whether or not it's passing
+  /* updateColor updates the color of background cell as user chooses a color, also updates contrast ratio and whether or not it's passing */
   function updateColor(color) {
     var hexColor = "transparent";
     if (color) {
